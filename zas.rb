bef1a1f4a0979da52a265f64f5264db56b93b8af
Juan Ibiapina <juanibiapina@gmail.com>
2015-09-27 18:17:43 -0300

    Update to zas 0.11.0

--- zas.rb
+++ zas.rb
@@ -1,11 +1,13 @@
 class Zas < Formula
   desc "Local development made easy"
   homepage ""
-  url "https://github.com/juanibiapina/zas/releases/download/v0.10.0/zas-v0.10.0.Darwin.tar.gz"
-  version "0.10.0"
-  sha256 "d772eaf081eb53657af3cfe7cce0ba1fd0d6d6aa59481fb8ca82da7aeae809db"
+  url "https://github.com/juanibiapina/zas/releases/download/v0.11.0/zas-v0.11.0.Darwin.tar.gz"
+  version "0.11.0"
+  sha256 "7d19f671d98e6e119fa09ac6aa2c57853b8db4d6a259e4becea17f6a9bae1d3d"
 
   def install
+    libexec.install Dir["libexec/*"]
     bin.install "bin/zasd"
+    bin.install "bin/zas"
   end
 end
