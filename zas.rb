9511016ed6e3efc072100fa50cb4e3d6507298b0
Juan Ibiapina <juanibiapina@gmail.com>
2016-03-13 21:34:57 -0300

    Update to zas version 0.16.0

--- zas.rb
+++ zas.rb
@@ -1,9 +1,9 @@
 class Zas < Formula
   desc "Local development made easy"
-  homepage ""
-  url "https://github.com/juanibiapina/zas/releases/download/v0.15.0/zas-v0.15.0.Darwin.tar.gz"
-  version "0.15.0"
-  sha256 "ea06a85bbbf7cbf22b0c4a808f54ea7f0f2e1aaf9634dd3f6ba62072bf093dae"
+  homepage "https://github.com/juanibiapina/zas"
+  url "https://github.com/juanibiapina/zas/releases/download/v0.16.0/zas-v0.16.0-x86_64-apple-darwin.tar.gz"
+  version "0.16.0"
+  sha256 "7caf22e7fb407ac0473e06f00606abf40cc45ac6ea9833b1e0f093c7aee2451d"
 
   def install
     prefix.install Dir["resources"]
