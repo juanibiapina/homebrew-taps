253f7a16ddece614927b41dd49d3debd7e567936
Juan Ibiapina <juanibiapina@gmail.com>
2018-03-25 22:03:04 +0200

    Update to zas version 0.21.0

--- zas.rb
+++ zas.rb
@@ -1,9 +1,9 @@
 class Zas < Formula
   desc "Simple router for local web development"
   homepage "https://github.com/juanibiapina/zas"
-  url "https://github.com/juanibiapina/zas/releases/download/v0.20.0/zas-v0.20.0-x86_64-apple-darwin.tar.gz"
-  version "0.20.0"
-  sha256 "f4e72458d227a384355290cbfd5a91a6a20cbbf98384356c01969407eee8d734"
+  url "https://github.com/juanibiapina/zas/releases/download/v0.21.0/zas-v0.21.0-x86_64-apple-darwin.tar.gz"
+  version "0.21.0"
+  sha256 "d624b2de23c924eedfa3aea8a9d934d650a7c6fe628f8c53e4f55fb8074d38fe"
 
   def install
     bin.install "zas"
