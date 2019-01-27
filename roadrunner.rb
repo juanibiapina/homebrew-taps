f8d747c6623d39a031ea7669fc1b1f743cd0a8e1
Juan Ibiapina <juanibiapina@gmail.com>
2019-01-27 14:02:48 +0100

    Update roadrunner to v2.1.0

--- roadrunner.rb
+++ roadrunner.rb
@@ -1,9 +1,9 @@
 class Roadrunner < Formula
   desc 'A fast and configurable prompt for shells'
   homepage 'https://github.com/juanibiapina/roadrunner'
-  url 'https://github.com/juanibiapina/roadrunner/releases/download/v2.0.0/roadrunner-v2.0.0-x86_64-apple-darwin.tar.gz'
-  version 'v2.0.0'
-  sha256 '8fc0b9c2981d6c32e5835d7cfb2e0d82b7492ff5f2e1ba9cb2fe8909af04ce20'
+  url 'https://github.com/juanibiapina/roadrunner/releases/download/v2.1.0/roadrunner-v2.1.0-x86_64-apple-darwin.tar.gz'
+  version 'v2.1.0'
+  sha256 '1ec45eb77dd3113aa12200ae0732678d072f4c78dd86067886811a376a8a4239'
 
   def install
     bin.install 'roadrunner'
