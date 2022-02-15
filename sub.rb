fd14bf761b1024ea040d84a13b5d43d4da2dcaaa
Juan Ibiapina <juanibiapina@gmail.com>
2022-02-15 17:35:37 +0100

    Update sub

--- sub.rb
+++ sub.rb
@@ -1,10 +1,9 @@
 class Sub < Formula
   desc 'Organize groups of scripts into documented CLIs with subcommands'
   homepage 'https://github.com/juanibiapina/sub'
-  url 'https://github.com/juanibiapina/sub/releases/download/v0.2.0/sub-v0.2.0-x86_64-apple-darwin.tar.gz'
-  version 'v0.2.0'
-  sha256 '4e2e8f08081a25481bac3cbd080acf48d034107ddee86beb7c430de625f3333c'
-
+  url 'https://github.com/juanibiapina/sub/releases/download/v0.7.0/sub-v0.7.0-x86_64-apple-darwin.tar.gz'
+  version 'v0.7.0'
+  sha256 'ef96faec1a9722d065b45f86e82922d2ee476ab1e15b501154a11e7f0c7810f6'
   def install
     bin.install 'sub'
   end
