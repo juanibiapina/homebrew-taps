7686869814c7b30db281f2536dff05bb0c193793
Juan Ibiapina <juanibiapina@gmail.com>
2023-03-16 19:46:51 +0100

    Update antr to 1.0.1

--- antr.rb
+++ antr.rb
@@ -1,9 +1,9 @@
 class Antr < Formula
   desc 'A simple to use file system event watcher that runs arbitrary commands'
   homepage 'https://github.com/juanibiapina/antr'
-  url 'https://github.com/juanibiapina/antr/releases/download/v0.3.0/antr-v0.3.0-x86_64-apple-darwin.tar.gz'
-  version 'v0.3.0'
-  sha256 '938b811980b44b42c4f5ccd6fb955d7b3559781d2dc02c9b6783b19b52e3277a'
+  url 'https://github.com/juanibiapina/antr/releases/download/v1.0.1/antr-v1.0.1-x86_64-apple-darwin.tar.gz'
+  version 'v1.0.1'
+  sha256 '72f506c3db62e60a93acbd4e9babf6e53b975eec98a93a239297aaa7d1b1b26d'
 
   def install
     bin.install 'antr'
