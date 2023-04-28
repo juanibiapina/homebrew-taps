598ec2b53ddbe984a8970358b5e73640b3c8d274
Juan Ibiapina <juanibiapina@gmail.com>
2023-04-28 17:38:37 +0200

    Update `sub` formula to v0.9.0

--- sub.rb
+++ sub.rb
@@ -1,9 +1,9 @@
 class Sub < Formula
   desc 'Organize groups of scripts into documented CLIs with subcommands'
   homepage 'https://github.com/juanibiapina/sub'
-  url 'https://github.com/juanibiapina/sub/releases/download/v0.8.1/sub-v0.8.1-x86_64-apple-darwin.tar.gz'
-  version 'v0.8.1'
-  sha256 '1d7c8b7e08319492e319c93e638bc91ccbc8b58cd9ec78807eee1763a1f800f3'
+  url 'https://github.com/juanibiapina/sub/releases/download/v0.9.0/sub-v0.9.0-aarch64-apple-darwin.tar.gz'
+  version 'v0.9.0'
+  sha256 'bec2748a5c1ef07302bb6540a8f40425aa8f1baa32585a0e0c23978416a3f9e1'
 
   def install
     bin.install 'sub'
