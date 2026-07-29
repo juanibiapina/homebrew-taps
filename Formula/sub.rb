74207a4abed5afe7804b4546d21451d23152c720
Juan Ibiapina <juanibiapina@gmail.com>
2026-07-29 10:48:00 +0200

    Brew formula update for sub version v2.3.1

--- Formula/sub.rb
+++ Formula/sub.rb
@@ -1,9 +1,9 @@
 class Sub < Formula
   desc 'Organize groups of scripts into documented CLIs with subcommands'
   homepage 'https://github.com/juanibiapina/sub'
-  url 'https://github.com/juanibiapina/sub/releases/download/v2.3.0/sub-v2.3.0-aarch64-apple-darwin.tar.gz'
-  version 'v2.3.0'
-  sha256 '37b488fa161b516a74e0499a31e1086266de0697719e0fe3525fe520e862a745'
+  url 'https://github.com/juanibiapina/sub/releases/download/v2.3.1/sub-v2.3.1-aarch64-apple-darwin.tar.gz'
+  version 'v2.3.1'
+  sha256 '82018689979d35134eb34fcf771a69d557a3dc8cb42a025d263e6d3db6db4442'
 
   def install
     bin.install 'sub'
